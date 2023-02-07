d00989e871bfb13154e7c51fad04fe1372a85a7d
RobinSingh <robinsingh@apples-MacBook-Pro.local>
2023-02-07 15:35:05 +0530

    second commit

---
+++ pages/typography.js
@@ -0,0 +1,25 @@
+import { Typography } from "@mui/material"
+
+function typography(){
+    return(
+        <>
+            <Typography variant="h1">Heading 1</Typography>
+            <Typography variant="h2">Heading 2</Typography>
+            <Typography variant="h3">Heading 3</Typography>
+            <Typography variant="h4">Heading 4</Typography>
+            <Typography variant="h5" gutterBottom>Heading 5</Typography>
+            <Typography variant="h6">Heading 6</Typography>
+
+            <Typography variant="subtitle1">Subtitle 1</Typography>
+            <Typography variant="subtitle2">Subtitle 2</Typography>
+
+            <Typography variant="body1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography>
+            <Typography variant="body2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography>
+
+            <Typography variant="h3" component='h1' gutterBottom>Heading 1</Typography>
+
+        </>
+    )
+}
+
+export default typography
